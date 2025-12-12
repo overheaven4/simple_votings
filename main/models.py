5c3ff4da8c57aa6001dc5efe94d6a54514f03ef1
overheaven <abv1550@gmail.com>
2025-12-12 19:47:52 +0300

    база данных

--- main/models.py
+++ main/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Voting(models.Model):
+    """Модель для хранения информации о голосовании"""
+    title = models.CharField(max_length=200)
+    description = models.TextField(blank=True)
+    created_at = models.DateTimeField(default=timezone.now)
+    creator = models.ForeignKey(to=User, on_delete=models.CASCADE)
+
+
+class VotingOption(models.Model):
+    """Модель для хранения информации о варианте голосования"""
+    voting = models.ForeignKey(to=Voting, on_delete=models.CASCADE)
+    text = models.CharField(max_length=200)
+
+
+class Vote(models.Model):
+    """Модель для хранения информации о голосе пользователя"""
+    option = models.ForeignKey(to=VotingOption, on_delete=models.CASCADE)
+    user = models.ForeignKey(to=User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(default=timezone.now)
