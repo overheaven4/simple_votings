f364ab21088ee1a85b92095de6d6f88be6d4f2d1
77 <77@gmail.com>
2025-12-12 20:16:06 +0300

    АвТоРиЗаЦиЯ

--- main/views.py
+++ main/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.contrib.auth import authenticate, login
+
+
+def my_view(request):
+    username = request.POST["username"]
+    password = request.POST["password"]
+    user = authenticate(request, username=username, password=password)
+    if user is not None:
+        login(request, user)
+        return login("auth")
+        ...
+    else:
+        # Return an 'invalid login' error message.
+        ...
