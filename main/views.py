db4d146e25ba653593cef2d684a3d8f3577f12e0
Arakel <oficcianiger@gmail.com>
2025-12-19 20:18:32 +0300

    АвТоРиЗаЦиЯ

--- main/views.py
+++ main/views.py
@@ -1,7 +1,17 @@
-from django.shortcuts import render
+import datetime
 
-# Create your views here.
+from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+
 from django.contrib.auth import authenticate, login
+def index_page(request):
+    context = {
+       print("Войдите в аккаунт")
+    }
+    return render(request, "index.html", context)
+
+
 
 
 def my_view(request):
@@ -11,7 +21,14 @@ def my_view(request):
     if user is not None:
         login(request, user)
         return login("auth")
-        ...
+
     else:
         # Return an 'invalid login' error message.
-        ...
+        return render(request,"login.html")
+        print("Did you forget your username or password?Are you stupied?")
+from django.contrib.auth import logout
+
+def logout_view(request):
+    logout(request)
+    return redirect('/')
+# Create your views here.
--- simple_votings/urls.py
+++ simple_votings/urls.py
@@ -16,10 +16,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from main.views import index_page, logout_view
 from main.views import my_view
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("auth/", my_view)
+    path("auth/", my_view),
+    path('login/', auth_views.LoginView.as_view()),
+    path('logout/', logout_view)
 ]
