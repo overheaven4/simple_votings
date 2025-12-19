cbdbb5dccb055fe5136dbfb2c1b97d3947b53e50
overheaven <abv1550@gmail.com>
2025-12-19 20:15:41 +0300

    базовая отрисовка голосования

--- main/views.py
+++ main/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.models import User
+from main.models import Voting, VotingOption, Vote
+from django import forms
 
-# Create your views here.
+
+def voting(request, voting_id):
+    voting = get_object_or_404(Voting, id=voting_id)
+    voting_options = VotingOption.objects.filter(voting_id=voting_id)
+    options = []
+    for i in range(len(voting_options)):
+        votes = Vote.objects.filter(option_id=voting_options[i].id)
+        options.append((voting_options[i], len(votes)))
+    context = {
+        "voting": voting,
+        "voting_options" : options,
+    }
+    return render(request, "voting.html", context)
