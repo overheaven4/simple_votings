454d733c6b95fc62d35901fdde1efa1323b62b45
overheaven <abv1550@gmail.com>
2025-12-23 01:11:33 +0300

    хотфикс Выбор голосов и запись голоса в БД

---
+++ main/forms/forms.py
@@ -0,0 +1,19 @@
+from django import forms
+from django.db.models import Count
+from main.models import Voting, VotingOption
+
+class VotingForm(forms.Form):
+    option = forms.ModelChoiceField(
+        queryset=None,
+        widget=forms.RadioSelect,
+        label="Time to pick"
+    )
+    def __init__(self, *args, **kwargs):
+
+        voting = kwargs.pop('voting', None)
+        super().__init__(*args, **kwargs)
+        if voting:
+            self.fields['option'].queryset = VotingOption.objects.filter(voting=voting).annotate(
+                votes_count = Count('vote')
+            )
+            self.fields['option'].label_from_instance = lambda obj: f"{obj.text} ({obj.votes_count})"
--- main/views.py
+++ main/views.py
@@ -2,17 +2,22 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.models import User
 from main.models import Voting, VotingOption, Vote
 from django import forms
-
+from main.forms.forms import VotingForm
 
 def voting(request, voting_id):
     voting = get_object_or_404(Voting, id=voting_id)
-    voting_options = VotingOption.objects.filter(voting_id=voting_id)
-    options = []
-    for i in range(len(voting_options)):
-        votes = Vote.objects.filter(option_id=voting_options[i].id)
-        options.append((voting_options[i], len(votes)))
     context = {
         "voting": voting,
-        "voting_options" : options,
     }
+
+    if request.method == "POST":
+        form = VotingForm(request.POST, voting=voting)
+        context['form'] = form
+        if form.is_valid():
+            option = form.cleaned_data['option']
+            Vote.objects.create(user=request.user, option=option)
+
+    else:
+        form = VotingForm(voting=voting)
+        context['form'] = form
     return render(request, "voting.html", context)
