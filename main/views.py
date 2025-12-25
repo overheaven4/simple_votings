3a82a3e093ec2f576032b51f4f0fecfec7033e50
overheaven <abv1550@gmail.com>
2025-12-26 02:01:12 +0300

    ограничение 1 юзер = 1 голос

--- main/views.py
+++ main/views.py
@@ -9,15 +9,36 @@ def voting(request, voting_id):
     context = {
         "voting": voting,
     }
-
-    if request.method == "POST":
-        form = VotingForm(request.POST, voting=voting)
-        context['form'] = form
-        if form.is_valid():
-            option = form.cleaned_data['option']
-            Vote.objects.create(user=request.user, option=option)
+    options_list = VotingOption.objects.filter(voting=voting)
+    is_voted = False
+    for option in options_list:
+        vote_list = Vote.objects.filter(option=option)
+        for vote in vote_list:
+            if vote.user == request.user:
+                is_voted = True
+                context['given_vote'] = vote.option
+                break
+        if is_voted: break
+    if is_voted:
+        template_name = "voting_result.html"
+        options = []
+        for i in range(len(options_list)):
+            votes_count = Vote.objects.filter(option=options_list[i]).count()
+            options.append((options_list[i], votes_count))
+        context['options_list'] = options
 
     else:
-        form = VotingForm(voting=voting)
+        template_name = "voting.html"
+        if request.method == "POST":
+            form = VotingForm(request.POST, voting=voting)
+            if form.is_valid():
+                option = form.cleaned_data['option']
+                Vote.objects.create(user=request.user, option=option)
+                return redirect(f'/voting/{voting_id}')
+
+        else:
+            form = VotingForm(voting=voting)
         context['form'] = form
-    return render(request, "voting.html", context)
+    return render(request, template_name, context)
+
+
