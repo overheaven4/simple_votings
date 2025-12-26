5a0be317ec1da90ca9c84e8372d20aa8cfce3633
pablo <kwefhkfewhufwehuewfhwefjhnwefhdwquhdwqijhofwnioswdhio@gmail.com>
2025-12-26 20:19:58 +0300

    СоЗдАнИе ГоЛоСоВаНиЯ

--- simple_votings/urls.py
+++ simple_votings/urls.py
@@ -17,6 +17,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from main.views import new_vote_page
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('new/', new_vote_page)
 ]
